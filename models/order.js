da4f26493a06e8e7a71598638c4cc568ad14bea3
Joshua Adewole <adewole.josh@gmail.com>
2022-05-19 11:07:38 +0100

    - feat: buyers viewing list of sellers catalogue done.

--- models/order.js
+++ models/order.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
     buyer: {type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true, required: true},
+    seller: {type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true, required: true},
     products:  [{type: mongoose.Schema.Types.ObjectId, ref: 'Products', unique: true, required: true}],
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 })
--- routes/buyers_route.js
+++ routes/buyers_route.js
@@ -1,7 +1,10 @@
 
 const User = require("../models/user");
+const Catalog = require("../models/catalogue");
 const express = require('express');
 const { buyerPerm } = require("../middlewares/permission");
+const Product = require("../models/product");
+const Order = require("../models/order");
 
 
 // only takes in json.
@@ -19,34 +22,80 @@ const listOfSellers = async function(req, res) {
     } catch (err) {
         console.log(err);
     }
-}
+};
 
-const  catalogOfSellers = async function(req, res) {
+const catalogOfSellers = async function(req, res) {
     try {
+        const { sellerId } = req.params.seller_id;
 
+        cur_seller = await User.findOne({user_id: sellerId});
+
+        sellerCat = await Catalog.findOne({seller: cur_seller['_id']})
+        .then(data => {
+            return data;
+        });
+        if (sellerCat){
+            return res.status(200).send(sellerCat);
+        } else {
+            return res.status(404).send('{ "message": "catalog does not exist!"}');
+        }
     } catch (err) {
-
+        console.log(err);
     }
-}
+};
 
-const  orderList = async function(req, res) {
+const createOrders = async function(req, res) {
     try {
+        console.log(req.headers);
+        const { sellerId } = req.params.seller_id;
+        const { products } = req.body;
 
+        console.log(sellerId, products);
+
+        if (!products) {
+            return res.status(400).send("You have not selected any products from this catalogue");
+        }
+        
+
+        cur_seller = await User.findOne({user_id: sellerId});
+        
+        sellerCatID = await Catalog.findOne({seller: cur_seller['_id']})
+        .then(data => {
+            return data['_id'];
+        });
+
+        for (elem in products) {
+            product = await Product.findOne({_id: products['id'][elem]})
+            .then(data => {
+                return data;
+            });
+            if(sellerCatID['products'].indexOf(product === -1) ){
+                return res.status(404).send("{'message': 'one or more products not in this category'");
+            }
+        }
+
+        new_order = Order.create({
+            buyer: req.user['user_id'],
+            seller: cur_seller,
+            products: products,
+        })
+
+        return res.status(201).json(new_order);
     } catch (err) {
-
+        console.log(err);
     }
-}
+};
 
 
 const router = express.Router();
 
-router.route('/list-of-sellers')
+router.route('/list-of-sellers/')
 .get(buyerPerm, listOfSellers);
 
-router.route('/seller-catalog/:seller_id')
+router.route('/seller-catalog/:seller_id/')
 .get(buyerPerm, catalogOfSellers);
 
-router.route('/create-order/:seller_id')
-.post(buyerPerm, orderList);
+router.route('/create-order/:seller_id/')
+.post(buyerPerm, createOrders);
 
 module.exports =  router;
